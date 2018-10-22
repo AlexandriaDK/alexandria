3226ec7ddc6af82310d5c2c5f51e23b89c729a27
Peter Brodersen <peter@ter.dk>
2018-10-23 01:05:38 +0200

    Google Assistant Actions tests

---
+++ www/services/actions/index.php
@@ -0,0 +1,80 @@
+<?php
+chdir("../../");
+require("./connect.php");
+require("base.inc");
+
+$postdata = file_get_contents("php://input");
+
+$r = json_decode($postdata);
+
+$project = 'alexandria-1489351053036';
+$responseId = $r->responseId;
+$session = $r->session;
+$intentName = $r->queryResult->intent->displayName;
+$languageCode = $r->queryResult->languageCode;
+
+$json_raw_output ='
+{
+  "fulfillmentText": "This is a text response.",
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
+          }
+        ]
+      }
+    }
+  ],
+  "source": "alexandria.dk",
+  "payload": {
+    "google": {
+      "expectUserResponse": true,
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": "this is a simple response"
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": "Hello, Facebook!"
+    },
+    "slack": {
+      "text": "This is a text response for Slack."
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/' . $project . '/agent/sessions/' . $session . '/contexts/context name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "' . $languageCode . '",
+    "parameters": {
+      "param": "param value"
+    }
+  }
+}
+';
+
+header("Content-Type: application/json");
+print $json_raw_output;
+
+# Log
+doquery("INSERT INTO actions_log (incoming_raw, outgoing_raw, responseid, session, intent, language, logtime) values ('" . dbesc($postdata) . "','" . dbesc($json_raw_output) . "','" . dbesc($responseId) . "','" . dbesc($session) . "','" . dbesc($intentName) . "','" . dbesc($languageCode) . "', NOW() )");
+
+?>
---
+++ www/services/actions/test.php
@@ -0,0 +1,17 @@
+<?php
+chdir("../../");
+require("./connect.php");
+require("base.inc");
+
+$postdata = getone("SELECT incoming_raw FROM actions_log WHERE id = 2");
+
+$r = json_decode($postdata);
+
+print_r($r);
+
+$intentName = $r->queryResult->intent->displayName;
+$languageCode = $r->queryResult->languageCode;
+
+print $intentName . PHP_EOL;
+print $languageCode . PHP_EOL;
+
