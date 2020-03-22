1c9d55c4a86405e88ea05f048bf490fe77d5875f
Peter Brodersen <peter@ter.dk>
2020-03-22 03:09:10 +0100

    404 magic redirect fix

--- www/error/404magic.php
+++ www/error/404magic.php
@@ -4,20 +4,21 @@ $string = substr($_SERVER['REQUEST_URI'],1);
 $magic = TRUE;
 
 if (is_dir($_SERVER['DOCUMENT_ROOT']."/$string")) {
-	$url = "http://{$_SERVER['HTTP_HOST']}/$string/";
+	$url = "/$string/";
 } elseif (preg_match('/^[a-z]{2}$/', $string) ) { //language redirect
 	$url = '/' . $string . '/';
 } elseif (preg_match('/^[a-z]{2}\/$/', $string) ) { //language not found
 	$magic = FALSE;
 } elseif (substr($string,-4) != '.htm' && substr($string,-5) != '.html') {
 	if (preg_match('_^[a-z]{2}/_', $string) ) {
-		$langpath = substr($string,0,2) . '/';
+		$langpath = "/" . substr($string,0,2) . '/';
 		$find = substr($string,3);
 	} else {
 		$langpath = '/';
 		$find = $string;
 	}
-	$url = "https://" . $_SERVER['HTTP_HOST'] . "/" . $langpath . "find?find=" . $find;
+	$url = $langpath . "find?find=" . $find;
+	#$url = "https://" . $_SERVER['HTTP_HOST'] . "/" . $langpath . "find?find=" . $find;
 } else {
 	$magic = FALSE;
 }
