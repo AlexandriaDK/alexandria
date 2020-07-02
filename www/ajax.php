cf7bc1cffebea18eddac2224559b1106460f4910
Peter Brodersen <peter@ter.dk>
2020-07-02 03:12:01 +0200

    Tag emoji for autosuggest

--- www/ajax.php
+++ www/ajax.php
@@ -22,7 +22,7 @@ if (strlen($query) >= 2) {
 		UNION ALL
 			SELECT convent.id, CONCAT(name,' (',year,')') AS label, 'convent' AS type, 'con' AS linkpart, 'convent' AS filepath, '' AS note FROM convent WHERE name LIKE '$escapequery%' OR CONCAT(name,' (',year,')') LIKE '$escapequery%' OR CONCAT(name,' ',year) LIKE '$escapequery%'
 		UNION ALL
-			SELECT tag, tag AS label, 'tag' AS type, 'tag' AS linkpart, 'tag' AS filepart, '(tag)' AS note FROM tags WHERE tag LIKE '$escapequery%' GROUP BY tag
+			SELECT tag, tag AS label, 'tag' AS type, 'tag' AS linkpart, 'tag' AS filepart, '🏷️ (tag)' AS note FROM tags WHERE tag LIKE '$escapequery%' GROUP BY tag
 		ORDER BY label
 	";
 
