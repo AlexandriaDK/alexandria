8fc313e33ec58fbf5ff1df118d97f4744ef09260
Peter Brodersen <peter@ter.dk>
2020-09-12 05:27:58 +0200

    HTML fixes for result, RPGFORUM admin

--- www/adm/rpgforum.php
+++ www/adm/rpgforum.php
@@ -5,6 +5,7 @@ chdir("..");
 require "rpgconnect.inc.php";
 require "base.inc.php";
 
+$postid = (int) $_GET['postid'];
 $search = trim((string) $_GET['search']);
 $author = trim((string) $_GET['author']);
 $order = (string) $_GET['order'];
@@ -12,13 +13,16 @@ $orderlist = ['score','title','timestamp'];
 
 
 function fixpost($post) {
+    $post = str_replace('<img src="../newpics/quote.gif">','<span style="font-size:1.5em;">"</span>', $post);
+    $post = str_replace('<img src="../pics/quote.gif">','<span style="font-size:1.5em;">"</span>', $post);
+    $content = $post;
     $doc = new DOMDocument();
     $doc->substituteEntities = false;
-    $content = mb_convert_encoding($post, 'html-entities', 'utf-8');
+    $content = mb_convert_encoding($content, 'html-entities', 'utf-8');
     $doc->loadHTML($content);
-    
     $result = $doc->saveHTML();
-    $result = str_replace('<img src="../newpics/quote.gif">','<span style="font-size:1.5em;">"</span>', $result);
+    $result = str_replace(['<html><body>','</body></html>'],'',$result);
+    $result = html_entity_decode($result);
     return $result;
 
 }
@@ -27,27 +31,30 @@ htmladmstart("RPGFORUM");
 
 print "<h1>RPGFORUM Archive search</h1>";
 
-print '<form action="rpgforum.php"><div>Search text: <input type="text" name="search" value="' . htmlspecialchars($search) . '"></div></form>';
-print '<form action="rpgforum.php"><div>Or author: <input type="text" name="author" value="' . htmlspecialchars($author) . '"></div></form>';
+print '<form action="rpgforum.php"><div>Search text: <input type="text" name="search" value="' . htmlspecialchars($search) . '"></div></form>' . PHP_EOL;
+print '<form action="rpgforum.php"><div>Or author: <input type="text" name="author" value="' . htmlspecialchars($author) . '"></div></form>' . PHP_EOL;
 
-if ($search !== '' || $author !== '') {
+if ($search !== '' || $author !== '' || $postid != 0) {
     print "<hr>";
     if( ! in_array($order,$orderlist) ) {
         $order = 'timestamp';
     }
     if ($search !== '') {
         $query = ("SELECT id, title, author, timestamp, views, post, MATCH(title,post) AGAINST('" . dbesc($search) . "' IN NATURAL LANGUAGE MODE) AS score FROM rpgforum_posts WHERE MATCH(title,post) AGAINST('" . dbesc($search) . "' IN NATURAL LANGUAGE MODE) ORDER BY $order DESC LIMIT 100");
-    } else {
+    } elseif ($author !== '') {
         $order = 'score desc, timestamp desc';
         $query = ("SELECT id, title, author, timestamp, views, post, MATCH(author) AGAINST('" . dbesc($author) . "' IN NATURAL LANGUAGE MODE) AS score FROM rpgforum_posts WHERE MATCH(author) AGAINST('" . dbesc($author) . "' IN NATURAL LANGUAGE MODE) ORDER BY $order LIMIT 100");
+    } else {
+        $query = ("SELECT id, title, author, timestamp, views, post FROM rpgforum_posts WHERE id = $postid");
     }
     $result = getall($query);
     foreach ($result AS $post) {
-        print '<div class="rpgforumpost">';
-        print '<h2 style="margin-bottom: 0">' . htmlspecialchars($post['title']) . '</h2>';
+        print '<div class="rpgforumpost">' . PHP_EOL;
+        print '<h2 style="margin-bottom: 0"><a href="rpgforum.php?postid=' . $post['id'] . '">' . htmlspecialchars($post['title']) . '</a></h2>' . PHP_EOL;
         print '<p style="font-weight: bold;">By <a href="rpgforum.php?author=' . rawurlencode($post['author']) . '">' . htmlspecialchars($post['author']) . "</a>, " . fulldatetime($post['timestamp']) . ", " . $post['views'] . " views.</p>";
         print fixpost($post['post']);
-        print "<hr>";
+        print '</div>' . PHP_EOL;
+        print "<hr>" . PHP_EOL . PHP_EOL;
     }
     if (count($result) == 100) {
         print "<p><i>Limit of 100 results has been reached</i></p>";
