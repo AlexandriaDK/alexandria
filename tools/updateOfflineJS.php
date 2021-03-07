44b33f1b8fee573d3777e889c2f186f5d6eab3f8
Peter Brodersen <peter@ter.dk>
2021-03-07 18:47:27 +0100

    offline updater: Sanity check

--- tools/updateOfflineJS.php
+++ tools/updateOfflineJS.php
@@ -1,5 +1,9 @@
 <?php
 $json = file_get_contents("https://alexandria.dk/en/export?dataset=all");
+if ( strlen($json) < 15000000) { // sanity check - must be at least 15 MB
+    exit(1);
+}
+
 $js = <<<EOD
 function loadAlexandria() {
 data = $json
