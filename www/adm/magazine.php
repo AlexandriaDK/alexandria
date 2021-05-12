19d6a4a40163c76ba89968a173716e58201260b2
Peter Brodersen <peter@ter.dk>
2021-05-12 22:09:11 +0200

    Delete contributors when deleting article

--- www/adm/magazine.php
+++ www/adm/magazine.php
@@ -171,6 +171,7 @@ if ($action == "changearticle" && $do != "Delete") {
 }
 
 if ($action == "changearticle" && $do == "Delete") {
+	doquery("DELETE FROM contributor WHERE article_id = $article_id");
 	$q = "DELETE FROM article WHERE id = $article_id";
 	$r = doquery($q);
 	if ($r) {
