85e38b57cfda7b30ea8e26d8ccad1a5463c73b4e
Peter Brodersen <peter@ter.dk>
2021-05-18 16:45:03 +0200

    Redaktion

--- www/adm/magazine.php
+++ www/adm/magazine.php
@@ -235,7 +235,7 @@ $(function() {
 		delay: 30,
 		minLength: 3
 	});
-	var peopleRoles = ['Skribent', 'Illustrator', 'Fotograf', 'Redaktør', 'Chefredaktør', 'Layout', 'Forfatter', 'Tegner', 'Anmelder', 'Brevkasseredaktør', 'Ansvarshavende redaktør', 'Lokalredaktion - Århus']
+	var peopleRoles = ['Skribent', 'Illustrator', 'Fotograf', 'Redaktør', 'Chefredaktør', 'Redaktion', 'Layout', 'Forfatter', 'Tegner', 'Anmelder', 'Brevkasseredaktør', 'Ansvarshavende redaktør', 'Lokalredaktion - Århus']
 	$( ".peopleroles" ).autocomplete({
 		source: peopleRoles,
 		autoFocus: true,
