35393343e54421c2503ec167997b26be68b1ad03
Thomas Mertz <thomas@tmertz.com>
2025-09-07 14:34:15 +0200

    En lille smule oprydning.

--- www/rpgconnect.inc.php
+++ www/rpgconnect.inc.php
@@ -1,14 +1,11 @@
 <?php
 require_once(__DIR__ . '/../includes/db.auth.php');
 
-$db_name = DB_NAME;
-$db_user = DB_USER;
-$db_pass = DB_PASS;
-$db_host = DB_HOST;
 $db_connector = DB_CONNECTOR;
 
 mysqli_report(MYSQLI_REPORT_OFF);
-if (!@($dblink = mysqli_connect($db_host, $db_user, $db_pass)) || @$_SERVER['QUERY_STRING'] == "crash") {
+
+if (!@($dblink = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306)) || @$_SERVER['QUERY_STRING'] == "crash") {
   define('DBERROR', TRUE);
   header("HTTP/1.1 503 Service Unavailable");
   header("X-Error: Database");
@@ -16,6 +13,6 @@ if (!@($dblink = mysqli_connect($db_host, $db_user, $db_pass)) || @$_SERVER['QUE
   $t->display('dberror.tpl');
   exit;
 }
-mysqli_select_db($dblink, $db_name) or die("Unable to select database\n"); // definitely need better error
+mysqli_select_db($dblink, DB_NAME) or die("Unable to select database\n"); // definitely need better error
 mysqli_set_charset($dblink, "utf8mb4");
 mysqli_query($dblink, "SET sql_mode = ''"); // allow dates such as 0000-00-00
